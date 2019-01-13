007087a1e8556663567113615f256d158b0934a3
Michael Yeates <michaeljyeates@gmail.com>
2019-01-14 00:26:09 +0100

    Initial commit

---
+++ eosio.lost.cpp
@@ -0,0 +1,182 @@
+
+
+#include "eosio.lost.hpp"
+
+void lostcontract::add(const ethereum_address& ethereum_address, name eos_address) {
+    /*require_auth(_self);
+
+    eosio_assert(is_account(eos_address), "Address does not exist");
+
+    eosio_assert(ethereum_address.length() == 42, "Ethereum address should have exactly 42 characters");
+
+    update_address(ethereum_address, [&](auto& address) {
+        address.ethereum_address = ethereum_address;
+        address.balance = balance;
+    });*/
+}
+
+
+void lostcontract::propose(name claimer) {
+        verifications_table verifications(_self, _self.value);
+        producers_table producers("eosio"_n, "eosio"_n.value);
+
+        auto verification = verifications.find(claimer.value);
+
+        eosio_assert(verification != verifications.end(), "Account not verified");
+
+        time_point_sec deadline{now() - WAITING_PERIOD};
+
+        eosio_assert(verification->added < deadline, "Thirty day waiting period has not passed");
+        eosio_assert(verification->proposed == 0, "Already proposed this lost key");
+
+
+        // Create transaction to be proposed
+
+        time_point_sec expire{now() + PROPOSAL_EXPIRY};
+
+        transaction trx;
+        trx.expiration = expire;
+        trx.ref_block_num = 0;
+        trx.ref_block_prefix = 0;
+
+        vector<eosiosystem::key_weight> keys;
+        eosiosystem::key_weight kw {
+                .key = verification->new_key,
+                .weight = (uint16_t) 1,
+        };
+        keys.push_back(kw);
+
+        eosiosystem::authority new_authority{
+                .threshold = (uint32_t) 1,
+                .keys = keys,
+                .accounts = {},
+                .waits = {}
+        };
+
+        action act_active = action(permission_level{verification->claimer, "owner"_n},
+                                   "eosio"_n, "updateauth"_n,
+                                   std::make_tuple(
+                                           verification->claimer,
+                                           "active"_n,
+                                           "owner"_n,
+                                           new_authority
+                                   ));
+
+        action act_owner = action(permission_level{verification->claimer, "owner"_n},
+                                   "eosio"_n, "updateauth"_n,
+                                   std::make_tuple(
+                                           verification->claimer,
+                                           "owner"_n,
+                                           name{0},
+                                           new_authority
+                                   ));
+
+        trx.actions.push_back(act_active);
+        trx.actions.push_back(act_owner);
+
+
+
+
+        // create a wrap transaction
+
+        transaction wrap;
+        wrap.expiration = expire;
+        wrap.ref_block_num = 0;
+        wrap.ref_block_prefix = 0;
+        vector<permission_level> perms;
+        perms.push_back(permission_level{"eosio"_n, "active"_n});
+        perms.push_back(permission_level{"eosio.wrap"_n, "active"_n});
+        wrap.actions.push_back(action(perms,
+                                          "eosio.wrap"_n, "exec"_n,
+                                          std::make_tuple(
+                                                  "eosio"_n,
+                                                  trx
+                                          )));
+
+
+
+
+        // Create the msig inline from here
+        // Get the top 30 bps for the requested list
+
+        vector<permission_level> requested;
+        auto by_votes = producers.get_index<"prototalvote"_n>();
+        auto prod_itr = by_votes.begin();
+
+//        requested.push_back(permission_level{_self, "active"_n});
+
+        if (prod_itr == by_votes.end()){
+                // testing purposes for unactivated chains
+                requested.push_back(permission_level{"eosio"_n, "active"_n});
+        }
+        else {
+                for (uint8_t i = 0;i<30;i++){
+                        requested.push_back(permission_level{prod_itr->owner, "active"_n});
+                        prod_itr++;
+                }
+        }
+
+        action(permission_level{_self, "active"_n},
+               "eosio.msig"_n, "propose"_n,
+               std::make_tuple(
+                       _self, // proposer
+                       verification->claimer, // proposal name
+                       requested,  // requested permissions
+                       wrap  // transaction
+               )).send();
+
+
+
+        // Mark this verification as proposed
+        verifications.modify(verification, same_payer, [&](verify_info &v){
+            v.proposed = 1;
+        });
+}
+
+void lostcontract::verify(std::vector<char> sig, name account, public_key newpubkey, name rampayer) {
+        require_auth(rampayer);
+
+        verifications_table verifications(_self, _self.value);
+
+        eosio_assert(is_account(account), "Account does not exist");
+
+        auto verification = verifications.find(account.value);
+
+        eosio_assert(verification == verifications.end(), "Account already verified");
+
+        verifications.emplace(rampayer, [&](verify_info &v){
+                v.claimer  = account;
+                v.added    = time_point_sec(now());
+                v.new_key  = newpubkey;
+                v.proposed = 0;
+        });
+}
+
+void lostcontract::reset(name claimer){
+        require_auth(_self);
+
+        verifications_table verifications(_self, _self.value);
+        auto verification = verifications.find(claimer.value);
+
+        eosio_assert(verification != verifications.end(), "Account not verified");
+
+        verifications.modify(verification, same_payer, [&](verify_info &v){
+            v.proposed = 0;
+        });
+}
+
+void lostcontract::clear(){
+        require_auth(_self);
+
+        verifications_table verifications(_self, _self.value);
+
+        auto itr = verifications.begin();
+        while (itr != verifications.end()){
+                itr = verifications.erase(itr);
+        }
+}
+
+
+EOSIO_DISPATCH( lostcontract,
+        (add)(propose)(verify)(reset)(clear)
+)
---
+++ eosio.lost.hpp
@@ -0,0 +1,122 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/multi_index.hpp>
+#include <eosiolib/transaction.hpp>
+
+using namespace eosio;
+using namespace std;
+
+typedef std::string ethereum_address;
+
+#ifndef WAITING_PERIOD
+#define WAITING_PERIOD 60 * 60 * 24 * 30
+#endif
+#ifndef PROPOSAL_EXPIRY
+#define PROPOSAL_EXPIRY 60 * 60 * 24 * 30
+#endif
+
+
+class [[eosio::contract("eosio.lost")]] lostcontract : public contract {
+
+private:
+
+	TABLE verify_info{
+			name              claimer;
+			time_point_sec    added;
+			public_key        new_key;
+			uint8_t           proposed;
+
+			uint64_t primary_key() const { return claimer.value; }
+
+			EOSLIB_SERIALIZE(verify_info,
+							(claimer)
+							(added)
+							(new_key)
+							(proposed))
+	};
+	typedef multi_index<"verified"_n, verify_info> verifications_table;
+
+public:
+
+	using contract::contract;
+
+	ACTION add(const ethereum_address &ethereum_address, name eos_address);
+
+	ACTION propose(name claimer);
+
+	ACTION verify(std::vector<char> sig, name account, public_key newpubkey, name rampayer);
+
+	ACTION reset(name claimer);
+
+	ACTION clear();
+};
+
+
+//Authority Structs
+namespace eosiosystem {
+
+	struct key_weight {
+		eosio::public_key key;
+		uint16_t weight;
+
+		// explicit serialization macro is not necessary, used here only to improve compilation time
+		EOSLIB_SERIALIZE(key_weight, (key)(weight))
+	};
+
+	struct permission_level_weight {
+		permission_level permission;
+		uint16_t weight;
+
+		// explicit serialization macro is not necessary, used here only to improve compilation time
+		EOSLIB_SERIALIZE(permission_level_weight, (permission)(weight))
+	};
+
+	struct wait_weight {
+		uint32_t wait_sec;
+		uint16_t weight;
+
+		// explicit serialization macro is not necessary, used here only to improve compilation time
+		EOSLIB_SERIALIZE(wait_weight, (wait_sec)(weight))
+	};
+
+	struct authority {
+
+		uint32_t threshold;
+		vector <key_weight> keys;
+		vector <permission_level_weight> accounts;
+		vector <wait_weight> waits;
+
+		EOSLIB_SERIALIZE(authority, (threshold)(keys)(accounts)(waits))
+	};
+}
+
+struct producer_info {
+	name owner;
+	double total_votes = 0;
+	eosio::public_key producer_key; /// a packed public key object
+	bool is_active = true;
+	std::string url;
+	uint32_t unpaid_blocks = 0;
+	time_point last_claim_time;
+	uint16_t location = 0;
+
+	uint64_t primary_key() const { return owner.value; }
+
+	double by_votes() const { return is_active ? -total_votes : total_votes; }
+
+	bool active() const { return is_active; }
+
+	void deactivate() {
+		producer_key = public_key();
+		is_active = false;
+	}
+
+	// explicit serialization macro is not necessary, used here only to improve compilation time
+	EOSLIB_SERIALIZE( producer_info, (owner)(total_votes)(producer_key)(is_active)(url)
+			(unpaid_blocks)(last_claim_time)(location)
+	)
+};
+
+typedef eosio::multi_index<"producers"_n, producer_info,
+		indexed_by<"prototalvote"_n, const_mem_fun < producer_info, double, &producer_info::by_votes> >
+>
+producers_table;
