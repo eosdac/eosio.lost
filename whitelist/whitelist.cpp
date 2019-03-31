cf84311768defbe93a750420e5e9135acb065e3a
Michael Yeates <michaeljyeates@gmail.com>
2019-04-01 01:05:17 +0300

    Use prods minor permissions for removing whitelist

--- whitelist/whitelist.cpp
+++ whitelist/whitelist.cpp
@@ -1,9 +1,9 @@
 #include "whitelist.hpp"
 
 void whitelist::add(name account, vector<char> eth_address) {
-    require_auth(_self);
+    require_auth(get_self());
 
-    whitelist_table whitelist(_self, _self.value);
+    whitelist_table whitelist(get_self(), get_self().value);
 
     eosio_assert(is_account(account), "Account does not exist");
 
@@ -17,9 +17,9 @@ void whitelist::add(name account, vector<char> eth_address) {
 }
 
 void whitelist::remove(name account) {
-    assert_minor_bp_auth();
+    require_auth(get_self());
 
-    whitelist_table whitelist(_self, _self.value);
+    whitelist_table whitelist(get_self(), get_self().value);
 
     eosio_assert(is_account(account), "Account does not exist");
 
@@ -30,9 +30,9 @@ void whitelist::remove(name account) {
 }
 
 void whitelist::clear(uint64_t count){
-    require_auth(_self);
+    require_auth(get_self());
 
-    whitelist_table whitelist(_self, _self.value);
+    whitelist_table whitelist(get_self(), get_self().value);
 
     uint16_t i = 0;
     auto itr = whitelist.begin();
@@ -42,28 +42,6 @@ void whitelist::clear(uint64_t count){
         itr = whitelist.erase(itr);
         i++;
     }
-}
-
-void whitelist::assert_minor_bp_auth(){
-
-
-    producers_table producers("eosio"_n, "eosio"_n.value);
-
-    auto idx = producers.get_index<"prototalvote"_n>();
-
-    uint8_t bp_auths = 0;
-    uint8_t required_auths = 8;
-    uint8_t bp_check = 0;
-
-    for ( auto it = idx.cbegin(); it != idx.cend() && 0 < it->total_votes && it->active() && bp_auths < required_auths && bp_check < 21; ++it ) {
-        if (has_auth(it->owner)){
-            bp_auths++;
-        }
-
-        bp_check++;
-    }
-
-    eosio_assert(bp_auths >= required_auths, "This action requires minor BP authentication");
 }
 
 
--- whitelist/whitelist.hpp
+++ whitelist/whitelist.hpp
@@ -14,10 +14,7 @@ class [[eosio::contract("whitelist")]] whitelist : public contract {
 private:
 #include "defs.hpp"
 
-    void assert_minor_bp_auth();
-
 public:
-
     using contract::contract;
 
     ACTION add(name address, vector<char> eth_address);
@@ -25,39 +22,4 @@ public:
     ACTION remove(name account);
 
     ACTION clear(uint64_t count);
-
-
 };
-
-
-struct producer_info {
-    name owner;
-    double total_votes = 0;
-    eosio::public_key producer_key; /// a packed public key object
-    bool is_active = true;
-    std::string url;
-    uint32_t unpaid_blocks = 0;
-    time_point last_claim_time;
-    uint16_t location = 0;
-
-    uint64_t primary_key() const { return owner.value; }
-
-    double by_votes() const { return is_active ? -total_votes : total_votes; }
-
-    bool active() const { return is_active; }
-
-    void deactivate() {
-        producer_key = public_key();
-        is_active = false;
-    }
-
-    // explicit serialization macro is not necessary, used here only to improve compilation time
-    EOSLIB_SERIALIZE( producer_info, (owner)(total_votes)(producer_key)(is_active)(url)
-            (unpaid_blocks)(last_claim_time)(location)
-    )
-};
-
-typedef eosio::multi_index<"producers"_n, producer_info,
-        indexed_by<"prototalvote"_n, const_mem_fun < producer_info, double, &producer_info::by_votes> >
->
-producers_table;
